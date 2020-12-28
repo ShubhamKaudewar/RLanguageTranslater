650f3a853df010709fa7952d40165bf16a99e300
ShubhamKaudewar <kaudewar@gmail.com>
2020-12-28 11:00:06 +0530

    typos_Updated

--- translator.r
+++ translator.r
@@ -24,24 +24,24 @@ language[["es"]] <- "Spanish"
 
 allow <- True
 
-while (allow):
+while (allow)
     user_code <- readline(prompt = "enter 'options' \n");
-    if (user_code == "options"):
+    if (user_code == "options"){
         print("Code : Language")
-        for (i in language.items()):
+        for (i in language.items())
             print("{i[0]} => {i[1]}")
         print()
-    else:
-        for (lan_code in language.keys()):
-            if (lan_code == user_code):
+        } else {
+        for (lan_code in language.keys())
+            if (lan_code == user_code)
                 print("You have selected {language[lan_code]}")
                 allow <- False
-        if (allow):
+        if (allow)
             print("It's not a valid language code!")
-
-while (True):
+        }
+while (True)
     string <- input("\nWrite the text you want to translate: \nTo exit the program write 'close'\n")
-    if (string == "close"):
+    if (string == "close")
         print("\nHave a nice Day!")
         break
 
@@ -50,6 +50,6 @@ while (True):
     print("\n{language[user_code]} translation: {translated.text}")
     print("Pronunciation : {translated.pronunciation}")
 
-    for (i in language.items()):
-        if (translated.src == i[0]):
+    for (i in language.items())
+        if (translated.src == i[0])
             print("Translated from : {i[1]}")
