57e2048f165f13cf821548d462dc9b1450130caa
amol2357 <amolmadnurkar2357@gmail.com>
2020-12-29 20:48:17 +0530

    debugging

--- translator.r
+++ translator.r
@@ -2,54 +2,54 @@ library(reticulate)
 source_python("pyfn.py")
 translator <- Translator()
 
-language <- list("bn"="Bangla",
-                 "bn"="Bangla",
-                 "en"="English",
-                 "ko"="Koren",
-                 "fr"="French",
-                 "de"="German",
-                 "he"="Hebrew",
-                 "hi"="Hindi",
-                 "it"="Italian",
-                 "ja"="Japanese",
-                 "ls"="Latin",
-                 "ms"="Malay",
-                 "ne"="Nepali",
-                 "ru"="Russian",
-                 "ar"="Arabic",
-                 "zh"="Chinese",
-                 "es"="Spanish")
+language <- list("bn=Bangla",
+                 "en=English",
+                 "ko=Koren",
+                 "fr=French",
+                 "de=German",
+                 "he=Hebrew",
+                 "hi=Hindi",
+                 "it=Italian",
+                 "ja=Japanese",
+                 "ls=Latin",
+                 "ms=Malay",
+                 "ne=Nepali",
+                 "ru=Russian",
+                 "ar=Arabic",
+                 "zh=Chinese",
+                 "es=Spanish")
 
 allow <- TRUE
-
+selected_language <- ''
 while (allow)
   user_code=readline(prompt="Enter 'options':")
-if (user_code == "options"){
-  print("Code: language")
-  keys<-key()
-  for (each in language)
-    print(each.split("=")[0],each.split("=")[1])
-  keys <- key(keys, each.split("=")[0])
-} else {
-  for (lan_code in keys)
-       if (lan_code == user_code)
-         print("You have selected {language[lan_code]}")
-       allow <- FALSE
-       if (allow)
-         print("It's not a valid language code!")
-}
+  print('user_Code')
+  if (user_code == "options"){
+    print("Code: language")
+    keys<-key()
+    values<-value()
+    for (each in language)
+      print(each.split('=')[0],each.split('=')[1])
+      keys <- key(keys, each.split('=')[0])
+      values <- values(values, each.split('=')[1])
+  } else {
+    for (i in 0:length(keys))
+      if (keys[i] == user_code)
+        print("You have selected",values[i])
+        selected_language<-values[i]
+      allow <- FALSE
+      if (allow)
+        print("It's not a valid language code!")
+  }
 while (TRUE)
   string=readline(prompt = "\nWrite the text you want to translate: \nTo exit the program write 'close'\n")
-if (string == "close")
-  print("\nHave a nice Day!")
-break
+  if (string == "close")
+    print("\nHave a nice Day!")
+  break
 
 translated = translator.translate(string, dest=user_code)
 
 print("\n{language[user_code]} translation: {translated.text}")
 print("Pronunciation : {translated.pronunciation}")
-
-for (i in language.items())
-  if (translated.src == i[0])
-    print("Translated from : {i[1]}")
+print("Translated from :",selected_language)
 
